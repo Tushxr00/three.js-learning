be7bad540780aa98c353a0fd1f125b9afccdeed4
Tushxr01 <tushar.nandy@squareboat.com>
2023-04-10 14:33:53 +0530

    FEAT: practice three js

--- src/components/canvas/Box.tsx
+++ src/components/canvas/Box.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { useBox } from "@react-three/cannon";
+import React, { useState } from "react";
 
 const Box = () => {
+  // const [ref, api] = useBox(() => ({ mass: 2, position: [0, 0, 0] }));
+  const [isRight, setIsRight] = useState(false);
   return (
-    <mesh>
-      <boxGeometry attach={"geometry"} />
+    <mesh
+      // ref={ref as any}
+      position={[2, 0, 1]}
+      // onClick={() => {
+      //   console.log({ isRight });
+      //   if (isRight) {
+      //     setIsRight(false);
+      //     api.velocity.set(2, 2, 2);
+      //   } else {
+      //     api.velocity.set(2, 2, 2);
+      //     setIsRight(true);
+      //     // api.velocity.set(-10, -10, 10);
+      //   }
+      // }}
+    >
+      <boxGeometry attach={"geometry"} args={[1, 1, 1]} />
       <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,63 @@ import styles from "@/styles/Home.module.css";
 import { Canvas } from "@react-three/fiber";
 import Box from "@/components/canvas/Box";
 import { OrbitControls, Stars } from "@react-three/drei";
+import Plane from "@/components/canvas/Plane";
+import { Physics } from "@react-three/cannon";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
   return (
-    <>
-      <Canvas style={{ width: "100vw", height: "100vh" }}>
-        <OrbitControls />
-        <Stars />
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
-        <Box />
-      </Canvas>
-    </>
+    <div
+      className="h-[100vh] w-[100vw] relative z-20"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "relative",
+        zIndex: 20,
+      }}
+    >
+      <div
+        className="h-full w-full z-10"
+        style={{
+          height: "100%",
+          width: "100%",
+          // position: "relative",
+          zIndex: 10,
+        }}
+      >
+        {" "}
+        Hello World !
+      </div>
+      <div
+        className="h-full w-full absolute top-0 left-0 z-0"
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          zIndex: 0,
+          top: 0,
+          left: 0,
+        }}
+      >
+        <Canvas
+          style={{ width: "100%", height: "100%" }}
+          camera={{ position: [10, 0, 10], fov: 45 }}
+          shadows
+          dpr={[1, 2]}
+        >
+          <OrbitControls />
+          <perspectiveCamera position={[0, 0, 2.5]} />
+          <Stars />
+          <ambientLight intensity={0.5} />
+          <spotLight position={[10, 15, 10]} angle={0.3} />
+          <Physics>
+            <Box />
+            <Plane />
+          </Physics>
+        </Canvas>
+      </div>
+    </div>
   );
 };
 
