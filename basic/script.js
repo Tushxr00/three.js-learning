81e6d38146f518dabaed2f7e3578e1b193a6aa70
Tushxr01 <tushar.nandy@squareboat.com>
2023-04-11 20:29:01 +0530

    FIX: three js used directly in react throwing error so using three js min script

---
+++ basic/script.js
@@ -0,0 +1,36 @@
+// import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+// RED cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+/**
+ * different ways to provide a color
+ * 0xff0000
+ * '#ff0000'
+ * 'red
+ */
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 500,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+console.log(canvas);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
